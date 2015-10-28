30f277a3eeb8cd50a12c5c6231815f26afc93ce4
徐佳楠 <jjyongsheng@gmail.com>
2015-10-28 10:56:40 +0800

    脚本监控翻页

--- src/DWD/DataBundle/Entity/CronJobDataRepository.php
+++ src/DWD/DataBundle/Entity/CronJobDataRepository.php
@@ -17,7 +17,7 @@ use Doctrine\ORM\EntityRepository;
  */
 class CronJobDataRepository extends EntityRepository
 {
-    public function getCronJobDataList($startTime, $endTime, $code, $owner)
+    public function getCronJobDataList($startTime, $endTime, $code = null, $owner = null, $start = 0, $count = 10, $name = null, $sortCol = null, $sortDir = null)
     {
         $em = $this->getEntityManager();
         $qb = $em->createQueryBuilder();
@@ -28,7 +28,10 @@ class CronJobDataRepository extends EntityRepository
             ->where('cd.startTime >= :startTime')
             ->andWhere('cd.startTime <= :endTime')
             ->setParameter('startTime', $startTime)
-            ->setParameter('endTime', $endTime);
+            ->setParameter('endTime', $endTime)
+            ->setFirstResult($start)
+            ->setMaxResults($count);
+
         if (isset($code) && $code != '') {
             if (intval($code) == 100) {
                 $qb
@@ -46,7 +49,58 @@ class CronJobDataRepository extends EntityRepository
                 ->andWhere('c.owner = :owner')
                 ->setParameter('owner', $owner);
         }
-
+        if (isset($name) && $name != '') {
+            $qb
+                ->andWhere($qb->expr()->like('cd.name', ':name'))
+                ->setParameter('name', $name);
+        }
+        if (isset($sortCol) && $sortCol != '' && isset($sortDir) && $sortDir != '') {
+            $qb
+                ->addOrderBy('cd.'.$sortCol, $sortDir);
+        }
         return $qb->getQuery()->getArrayResult();
+    }
+
+    public function getCronJobDataCount($startTime, $endTime, $code = null, $owner = null, $name = null)
+    {
+        $em = $this->getEntityManager();
+        $qb = $em->createQueryBuilder();
+
+        $qb
+            ->select('count(cd)')
+            ->from('DWD\DataBundle\Entity\CronJobData', 'cd')
+            ->where('cd.startTime >= :startTime')
+            ->andWhere('cd.startTime <= :endTime')
+            ->setParameter('startTime', $startTime)
+            ->setParameter('endTime', $endTime);
+
+        if (isset($code) && $code != '') {
+            if (intval($code) == 100) {
+                $qb
+                    ->andWhere('cd.flag = :flag')
+                    ->setParameter('flag', false);
+            } else {
+                $qb
+                    ->andWhere('cd.code = :code')
+                    ->setParameter('code', $code);
+            }
+        }
+        if (isset($owner) && $owner != '') {
+            $qb
+                ->leftJoin("cd.cronjob", 'c')
+                ->andWhere('c.owner = :owner')
+                ->setParameter('owner', $owner);
+        }
+        if (isset($name) && $name != '') {
+            $qb
+                ->andWhere($qb->expr()->like('cd.name', ':name'))
+                ->setParameter('name', $name);
+        }
+        $cronJobDataCount = $qb->getQuery()->getResult();
+        if( count($cronJobDataCount) ) {
+            return intval($cronJobDataCount[0][1]);
+        } else {
+            return 0;
+        }
     }
 }
--- src/DWD/TongjiBundle/Controller/MonitorCronJobController.php
+++ src/DWD/TongjiBundle/Controller/MonitorCronJobController.php
@@ -16,9 +16,10 @@ use Symfony\Component\HttpFoundation\Response;
 class MonitorCronJobController extends Controller
 {
     protected $_default_time_interval = 5184000;
+    protected $_cronjob_table_columns = array( 'name', 'job', 'msg', 'startTime', 'stopTime', 'cost', 'code' );
 
     /**
-     * Uri list of Analyse statistics
+     * Uri list of monitor cronjob statistics
      *
      * @Route("/",name="dwd_tongji_monitor_cronjob_dashboard")
      */
@@ -52,5 +53,55 @@ class MonitorCronJobController extends Controller
             'owner'             => $owner,
             'subject'           => 'tongji_monitor_cronjob'
         ));
+    }
+
+    /**
+     * JSON DATA of monitor cronjob statistics
+     *
+     * @Route("/list",name="dwd_tongji_monitor_cronjobs_show")
+     */
+    public function cronjobsDataAction(Request $request)
+    {
+        $iDisplayStart   = $request->get('iDisplayStart');
+        $iDisplayLength  = $request->get('iDisplayLength');
+        $sEcho           = $request->get('sEcho');
+        $sSearch         = $request->get('sSearch', null);
+        $iSortCol_0      = $request->get('iSortCol_0', null);
+        $sSortDir_0      = $request->get('sSortDir_0', null);
+        $startTime       = $request->get('startTime');
+        $endTime         = $request->get('endTime');
+        $code            = $request->get('code');
+        $owner           = $request->get('owner');
+
+        $cronjobDataRepository = $this->getDoctrine()->getRepository('DWDDataBundle:CronJobData');
+        $cronjobDataList = $cronjobDataRepository->getCronJobDataList($startTime, $endTime, $code, $owner, $iDisplayStart, $iDisplayLength, $sSearch, $this->_cronjob_table_columns[$iSortCol_0], $sSortDir_0);
+        $total = $cronjobDataRepository->getCronJobDataCount( $startTime, $endTime, $code, $owner, $sSearch );
+
+//        $cronjobList = [];
+//        foreach( $cronjobDataList as $cronjobDataKey => $cronjobDataValue ) {
+//            $cronjobList []= array(
+//                                $cronjobDataValue['name'],
+//                                $cronjobDataValue['job'],
+//                                $cronjobDataValue['msg'],
+//                                $cronjobDataValue['startTime'],
+//                                $cronjobDataValue['stopTime'],
+//                                $cronjobDataValue['cost'],
+//                                $cronjobDataValue['code'],
+//                                ''
+//                             );
+//        }
+
+
+
+        $res             = array
+        (
+            "sEcho"                => $sEcho,
+            "aaData"               => $cronjobDataList,
+            "iTotalRecords"        => $total,
+            "iTotalDisplayRecords" => $total,
+        );
+        $response        = new Response();
+        $response->setContent( json_encode( $res ) );
+        return $response;
     }
 }

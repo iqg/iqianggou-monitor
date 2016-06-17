0a41d33db62d36583d9fa3c502ce8f23adbd976f
wangpengpeng <1215303889@qq.com>
2016-06-17 10:41:15 +0800

    启明监控脚本

---
+++ src/DWD/DataBundle/Command/iqg_analyse_log/parse_openapi_access_log.php
@@ -0,0 +1,186 @@
+<?php
+
+require_once ('tools.php');
+
+class AccessLogParse
+{
+	private $_data = array();
+	private $_removedKey = array( 'content-type', 'content-length', 'web-server-type', 'cookie', 'accept-encoding', 'x-php-ob-level', 'accept', 'accept-language' );
+	private $_connection;
+	private $_subject = 'access';
+	private $_subjectParentPath = '';
+	private $_subjectPath = '';
+	private $_serverIp = '127.0.0.1';
+	private $_validHost = array( 'api.v3.iqianggou.com', 'm.iqianggou.com' );
+	private $_serverId = 0;
+	private $_config;
+
+	function __construct()
+	{
+		$this->_config = parse_ini_file("config.ini", true);
+		$this->_subject = $this->_config['openapi_access']['prefix_file_name'];
+		$this->_subjectParentPath = $this->_config['common']['subject_path'];
+		$this->_serverIp = $this->_config['common']['server_ip'];
+
+		$this->connectMongo($this->_config['mongo']['server'], $this->_config['mongo']['db_name']);
+	}
+
+	protected function connectMongo( $server = "mongodb://localhost:27017", $db_name = "iqianggou_analyse" )
+	{
+		$m = new MongoClient($server);
+		$this->_connection = $m->selectDB($db_name);
+	}
+
+	public function AnalyseRequestInfo( $requestInfo )
+	{
+		$this->_data = array();
+		if (false == isset( $requestInfo['request'] )) {
+			return false;
+		}
+		if ( false == isset( $requestInfo['request']['header']['host'][0] ) || false == in_array( $requestInfo['request']['header']['host'][0], $this->_validHost ) ) {
+			return false;
+		}
+		$request = $requestInfo['request'];
+		foreach ( $request as $key => $value ) {
+			if (in_array( $key, $this->_removedKey )) {
+				continue;
+			}
+			if ($key == 'header') {
+				foreach ( $value as $headerKey => $valueKey ) {
+					if (in_array( $headerKey, $this->_removedKey )) {
+						continue;
+					}
+					$this->_data['header'][$headerKey] = $valueKey[0];
+				}
+				continue;
+			}
+			$this->_data[$key] = $value;
+		}
+		if (isset( $requestInfo['ResponseStatusCode'] )) {
+			$this->_data['ResponseStatusCode'] = $requestInfo['ResponseStatusCode'];
+		}
+		if (isset( $requestInfo['cost'] )) {
+			$this->_data['cost'] = $requestInfo['cost'];
+		}
+		$this->_data['request_time'] = $requestInfo['request_time'];
+		$this->_data['server_ip'] = $this->_serverIp;
+		if (0 != $this->_serverId) {
+			$this->_data['server_ip'] = $this->_config['server_ip']['server' . $this->_serverId];
+		}
+		return true;
+	}
+
+	public function GetRequestInfo( $msg )
+	{
+		$pattern = '/^\[(.+)\] \[.+\]: \[(.+)\]/';
+		preg_match( $pattern, $msg, $matches );
+		if (count($matches) < 3) {
+			return false;
+		}
+		$currentTimestamp = strtotime( $matches[1] );
+		$requestInfo = json_decode( $matches[2], true );
+		$requestInfo['request_time'] = $currentTimestamp;
+		return $requestInfo;
+	}
+
+	public function GetYesterday()
+	{
+		return date("Y-m-d", strtotime("-1 day"));
+	}
+
+	public function GetYesterdayLogFile()
+	{
+		return $this->_subjectPath . $this->_subject . '-' . $this->GetYesterday() . '.log';
+	}
+
+	public function SetSubjectPath( $subServerFile )
+	{
+		if (preg_match('/[a-zA-Z_]+([0-9]+)/', $subServerFile, $matches)) {
+			$this->_serverId = intval($matches[1]);
+		}
+		$this->_subjectPath = $this->_subjectParentPath . ($subServerFile . '/');
+	}
+
+	public function SaveRequestInfo()
+	{
+		$coll = $this->_connection->openapi_access_logs;
+		$coll->insert( $this->_data );
+	}
+
+	public function RunAction( $fileName )
+	{
+		$fp 			= fopen( $fileName, "r" );
+		$error			= error_get_last();
+		if (NULL != $error) {
+			exit('读取文件异常,系统退出');
+		}
+
+		echo "Start parse OpenAPI access log[" . $fileName . "] at" . date('Y-m-d H:i:s') . "\n";
+
+		while( !feof( $fp ) ) 
+		{ 
+			$lineMsg = fgets( $fp );
+			$requestInfo = $this->GetRequestInfo( $lineMsg );
+			if (false == $requestInfo) {
+				continue;
+			}
+			if ($this->AnalyseRequestInfo( $requestInfo )) {
+				$this->SaveRequestInfo();
+			}
+		}
+
+		echo "End parse OpenAPI access log[" . $fileName . "] at" . date('Y-m-d H:i:s') . "\n";
+
+		fclose( $fp );	
+	}
+
+	public function RunYesterdayLog()
+	{
+		$fileName 		= $this->GetYesterdayLogFile();
+		$this->RunAction($fileName);
+	}
+
+	public function RunCatelogLog()
+	{
+		$thisCatelog = scandir($this->_subjectPath);
+
+		foreach ( $thisCatelog as $thisFile ) {
+			if ( 0 == preg_match('/^access.+\.log/', $thisFile) ) {
+				continue;
+			}
+			$thisFilePath = $this->_subjectPath . $thisFile;
+			$this->RunAction($thisFilePath);
+		}
+	}
+
+	public function RunSpecificLog( $date )
+	{
+		$fileName       = $this->_subjectPath . $this->_subject . '-' . $date . '.log';
+		$this->RunAction($fileName);
+	}
+}
+
+try{
+	$startTime = time();
+	$job =  __dir__ . '/' . implode(" ", $argv);
+	$tool =  new Tool();
+	$tool->SetStartTime( $startTime );
+	$tool->SetJob( $job );
+	$tool->appendCrontabMonitorLog($tool->getCrontabMonitorLogBootstrapJSON('', 0));
+
+	$accessLogParse = new AccessLogParse();
+
+	foreach ( $argv as $key => $value ) {
+		if ( $key == 0 ) {
+			continue;
+		}
+		$accessLogParse->SetSubjectPath($value);
+		$accessLogParse->RunYesterdayLog();
+	}
+
+	$tool->appendCrontabMonitorLog($tool->getCrontabMonitorLogEndJSON('', 0));
+} catch (Exception $e) {
+	$tool->appendCrontabMonitorLog($tool->getCrontabMonitorLogEndJSON($e->getMessage(), 99));
+	exit();
+}
+
---
+++ src/DWD/DataBundle/Command/iqg_analyse_log/status_internalapi.php
@@ -0,0 +1,180 @@
+<?php
+
+class InternalApiStatus
+{
+	private $statusInfo =  []; //数据量比较大
+	public  $requestTime;
+	private $_connection;
+	private $_subject = 'iqg_access_internal_api.log';
+	private $_subjectPath = '';
+	private $_serverIp = '127.0.0.1';
+    private $_config;
+
+    function __construct(){
+        $this->_config = parse_ini_file("config.ini", true);
+
+		$this->_subject =  $this->_config['internalapi_status']['prefix_file_name'];
+        $this->_subjectPath = $this->_config['internalapi_access']['internalapi_subject_path'];
+		$this->_serverIp =  $this->_config['common']['server_ip'];
+
+		$this->connectMongo( $this->_config['mongo']['server'],  $this->_config['mongo']['db_name']);
+	}
+
+	protected function connectMongo($server = "mongodb://localhost:27017", $db_name = "iqianggou_analyse")
+	{
+		$m = new MongoClient($server);
+		$this->_connection = $m->selectDB($db_name);
+	}
+    //分析每个接口的数据
+	public function AnalysisApiInfo( $requestInfo, $source = null )
+	{
+    	$path = $requestInfo['path'];
+		$extend  = pathinfo($path);
+
+		if( isset( $extend["extension"] ) ){
+			$extend  = strtolower($extend["extension"]);
+			if( preg_match( "/jpg|png|js|css|webp|ts/", $extend ) ){
+				return ;
+			}
+		}
+ 
+        $path    = preg_replace( "/\/\d+/", "/:id", $path );
+
+        if( $requestInfo['cost'] < 0 ){
+        	return ;
+        }
+
+		if( isset( $this->statusInfo[$path] ) ){  //相同的接口第二次调用
+			++ $this->statusInfo[$path]['called'];//总的调用次数
+			$this->statusInfo[$path]['totalCost'] += $requestInfo['cost'];
+			$this->statusInfo[$path]['minCost'] > $requestInfo['cost'] ? $this->statusInfo[$path]['minCost'] = $requestInfo['cost']:true;
+			$this->statusInfo[$path]['maxCost'] < $requestInfo['cost'] ? $this->statusInfo[$path]['maxCost'] = $requestInfo['cost']:true;
+		} else {
+			$this->statusInfo[$path] 		       = array();
+			$this->statusInfo[$path]['path']       = $path;
+			$this->statusInfo[$path]['called']     = 1;
+			$this->statusInfo[$path]['totalCost']  = $requestInfo['cost'];
+			$this->statusInfo[$path]['minCost']    = $requestInfo['cost'];
+			$this->statusInfo[$path]['maxCost']    = $requestInfo['cost'];
+			$this->statusInfo[$path]['successed']  = 0;
+			$this->statusInfo[$path]['failed']     = 0;
+		}
+		if( isset( $requestInfo['ResponseStatusCode'] ) ){
+			 $this->StatusCodeVerify( $requestInfo['ResponseStatusCode'] ) ? ++ $this->statusInfo[$path]['successed'] : ++ $this->statusInfo[$path]['failed'];
+			// ++ $this->statusInfo[$path]['successed'];
+		} else {
+			++ $this->statusInfo[$path]['failed'];
+		}		
+	}
+
+	public function StatusCodeVerify( $code ){
+		if( intval( $code ) == 0 ){
+			return true;
+		}
+		return false;
+	}
+
+	public function Output()
+	{
+//		usort( $this->statusInfo, array( 'ApiStatus', 'calledCmpSort' ) );
+		usort( $this->statusInfo, array( 'InternalApiStatus', 'calledCmpSort' ) );
+		$txt = sprintf("|%50s| %12s |  %12s | %12s | %12s | %12s | %12s |\r\n", 'PATH', 'maxCost','minCost','avgCost','successed','failed','called' );
+		echo $txt;
+
+		foreach( $this->statusInfo as  $pathInfo ){
+			$txt = sprintf("| %50s | %12.2f | %12.2f | %12.2f | %12d | %12d | %12d |\r\n", $pathInfo['path'], $pathInfo['maxCost'], $pathInfo['minCost'], $pathInfo['totalCost'] / $pathInfo['called'], $pathInfo['successed'], $pathInfo['failed'], $pathInfo['called']);
+			echo $txt;
+		}
+	}
+
+	static public function calledCmpSort( $fir, $sec )
+	{
+		if( $fir['called'] == $sec['called'] ){
+			return 0;
+		}
+
+		return ($fir['called'] > $sec['called']) ? -1 : 1;
+	} 
+
+
+	public function CheckValidRequestTime( $leftRequiredTime = null, $rightRequiredTime = null )
+	{
+		if (isset( $leftRequiredTime )) {
+			$leftRequiredTime = strtotime($leftRequiredTime);
+			if ($this->requestTime < $leftRequiredTime)
+				return false;
+		}
+		if (isset( $rightRequiredTime )) {
+			$rightRequiredTime = strtotime($rightRequiredTime);
+			if ($this->requestTime > $rightRequiredTime)
+				return false;
+		}
+		return true;
+	}
+
+	public function GetYesterday()
+	{
+		return date("Y-m-d", strtotime("-1 day"));
+	}
+
+	public function GetYesterdayLogFile()
+	{
+		return $this->_subjectPath . $this->_subject . '-' . $this->GetYesterday() . '.log';
+	}
+	
+	public function SaveApiInfo( $startTimestamp )
+	{
+		$coll = $this->_connection->internalapi_access_data;//写的数据库
+
+		foreach ( $this->statusInfo as $pathInfo ) {
+			$pathInfo['startTimestamp'] = $startTimestamp;
+			$coll->insert( $pathInfo );
+		}
+	}
+    //入口,从mogodb里取得数据
+	public function GetApiStatus( $startTime = null, $endTime = null, $saveFlag = false )
+	{
+		$coll = $this->_connection->internalapi_access_logs;
+		$query = array();
+		if (isset( $startTime )) {
+			$startTime = strtotime($startTime);
+			$query['request_time']['$gte'] = intval($startTime); 
+		}
+		if (isset( $endTime )) {
+			$endTime = strtotime($endTime);
+			$query['request_time']['$lt'] = intval($endTime);
+		}
+		$cursor = $coll->find( $query );
+
+		while ($cursor->hasNext()) {
+			$requestInfo = $cursor->getNext();
+			$this->AnalysisApiInfo( $requestInfo, 'mongo' );
+		}
+		if ($saveFlag) {
+			$this->SaveApiInfo($query['request_time']['$gte']);
+		} else {
+			$this->Output();
+		}
+		$this->DropApiAccessLogs();
+	}
+
+	public function DropApiAccessLogs()
+	{
+		$coll = $this->_connection->internalapi_access_logs;
+		$response = $coll->drop();
+		return $response;
+	}
+}
+
+try{
+    ini_set('memory_limit','-1');
+    $startTime = time();
+    $apiStatus 		= new InternalApiStatus();
+    $apiStatus->GetApiStatus( $apiStatus->GetYesterday(), date("Y-m-d"), true );
+    //$apiStatus->GetApiStatus( "2015-09-16", "2015-09-17" );
+    $totalTime = time() - $startTime;
+    var_dump('总得处理时间:'. $totalTime);
+} catch (Exception $e) {
+    var_dump($e->getMessage(),$e->getCode());
+}
+

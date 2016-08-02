8410181df77c7ec03a44c14c50c64c6c35ab6adc
wangpengpeng <1215303889@qq.com>
2016-08-02 10:44:56 +0800

    添加软链接

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -39,6 +39,8 @@ namespace :deploy do
         run "ln -s #{deploy_to}/shared/config/parameters.yml #{release_path}/app/config/parameters.yml"
         run "rm    #{release_path}/src/DWD/DataBundle/Command/iqg_analyse_log/config.ini"
         run "ln -s #{deploy_to}/shared/config.ini #{release_path}/src/DWD/DataBundle/Command/iqg_analyse_log/config.ini"
+        run "ln -s #{deploy_to}/shared/hsq-internalapi/config.ini #{release_path}/src/DWD/DataBundle/Command/hsq_analyse_log/hsq-internalapi/config.ini"
+        run "ln -s #{deploy_to}/shared/hsq-openapi/config.ini #{release_path}/src/DWD/DataBundle/Command/hsq_analyse_log/hsq-openapi/config.ini"
         run "ln -s #{deploy_to}/shared/web/uploads #{release_path}/web/upload"
         run "cp -r #{deploy_to}/shared/vendor/ #{release_path}/"
         run "php #{release_path}/vendor/sensio/distribution-bundle/Sensio/Bundle/DistributionBundle/Resources/bin/build_bootstrap.php"
